325f57d5f7a7a2c01e180ab028c9e58e762029c5
masterLord <vavan7520@gmail.com>
2022-01-13 00:16:36 +0300

    first commit in to master

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', function () {
+    "use strict";
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        // spaceBetween: 130,
+        autoplay: {
+            delay: 2000,
+        },
+        speed: 500,
+        slidesPwgGroup: 1,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            // dynamicBullets: true,
+        },
+
+    });
+    
+
+
+    new WOW().init();
+
+});
+
+
