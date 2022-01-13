b4705fa7ec45e3be40f763e9b3332aeb882dd3ea
masterLord <vavan7520@gmail.com>
2022-01-13 19:58:19 +0300

    end

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,6 +19,65 @@ window.addEventListener('DOMContentLoaded', function () {
             // dynamicBullets: true,
         },
     });
+
+    // ! validation
+
+
+    let validateForms = function (selector, rules, successModal, yaGoal) {
+        new window.JustValidate(selector, {
+            rules: rules,
+
+            messages: {
+                name: {
+                    required: 'Введите имя пользователя',
+                    minLength: 'Слишком короткое Имя',
+
+                },
+                tel: {
+                    required: 'Укажите ваш телефон'
+                },
+            },
+
+
+            submitHandler: function (form) {
+                let formData = new FormData(form);
+                let xhr = new XMLHttpRequest();
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+
+                        if (xhr.status === 200) {
+                            console.log("Отправлено");
+                            // let modal = document.querySelector('.overlay');
+                            // modal.classList.add('thanks');
+                            // let closes = document.querySelector('.modal__close');
+                            // closes.addEventListener('click', function () {
+                            //     modal.classList.remove('thanks');
+                            // });
+                        }
+                    }
+                };
+                xhr.open('POST', 'mailer/smart.php', true);
+                xhr.send(formData);
+
+                form.reset();
+            },
+
+        });
+
+    };
+    validateForms('#form', {
+        name: {
+            required: true,
+            minLength: 2,
+        },
+        tel: {
+            required: true,
+
+        },
+    }, 'thanks-popup', 'send goal');
+    
+        
 });
 
 
